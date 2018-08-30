0ffb61de4b35cd9cfb2f5f86f4461d57e314a236
harshit1810 <hsrivastava816@gmail.com>
2018-08-30 09:33:51 +0530

    adding to master

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Classic Snake Game'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /^index.js$/,
+                exclude: '/node_modules|dist|webpack*/',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'playsnake.webpack.min.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
